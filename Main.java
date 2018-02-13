2afb562d1494666ca019e1f59abed2b54ffa8693
Steven Hernandez Rios <estevenhr@hotmail.com>
2018-02-12 21:32:02 -0500

    Clase principal
    Inicio del sistema

---
+++ Main.java
@@ -0,0 +1,34 @@
+public class Main
+{
+	//javac Main.class
+	//java Main
+	public static void main(String[] arg){
+
+		
+		System.out.println("\n\t\tHola mundo desde la universidad de cundinamarca");
+		System.out.println("Programaciòn II");
+		System.out.println("Steven Hernandez Rios");
+		System.out.println("");
+		System.out.println("");
+
+		System.out.println("\n\tSUMA DE DOS VARIABLES");
+
+		int numero1=20;
+		int numero2=30;
+
+		System.out.println("La suma del numero1 = "+numero1+" + numero2 = "+numero2+" = "+(numero1+numero2)); 
+
+		if(numero1>numero2){
+			System.out.println("El "+numero1+" es mayor que "+numero2);
+		}else{
+			System.out.println("El "+numero2+" es mayor que "+numero1);
+		}
+
+		String nombre ="Steven Hernandez";
+
+		System.out.println("Programa Terminado"+nombre);
+
+	}
+
+
+}
